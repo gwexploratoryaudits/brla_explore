94693c67aa33a1c642a98336ca5b7fcd32c1ce33
Poorvi Vora <poorvi@gwu.edu>
2020-05-09 22:45:15 -0400

    detailed pvalue tests

--- R2Audits/Athena/Scripts/2020MontgomeryPrimary/p_values_Montgomery.m
+++ R2Audits/Athena/Scripts/2020MontgomeryPrimary/p_values_Montgomery.m
@@ -6,54 +6,119 @@ num_tests = 27;
 alpha = 0.1;
 delta = 1; %default
 
-% Values below are chosen to be max round sizes for 0.9 percentiles, and
-% kmin+1, kmin, kmin-1
-n_in = [17, 17, 17, 42, 42, 42, 17, 17, 17, 49, 49, 49, 19, 19, 19, ...
-    9, 9, 9, 33, 33, 33, 33, 33, 33, 86, 86, 86];
+% Values below are chosen to be max round sizes for 0.9 percentiles for 
+% runnerup, and k=kmin+1, kmin, kmin-1. When there is more than one losing 
+% candidate, smaller number of votes for that candidate. 
+n_in = [17, 18, 19, 42, 42, 42, 17, 17, 17, 49, 49, 49, 19, 19, 19, ...
+    9, 11, 11, 39, 39, 40, 33, 33, 33, 86, 86, 86];
 
-k_in = [13, 12, 11, 27, 26, 25, 13, 12, 11, 31, 30, 29, 14, 13, 12, ...
-    8, 7, 6, 22, 21, 20, 22, 21, 20, 51, 50, 49];
+k_in{1} = [0, 13, 0, 0, 0, 0, 0, 4, 0, 0, 0]; 
+k_in{2} = [0, 12, 1, 0, 0, 0, 0, 5, 0, 0, 0]; 
+k_in{3} = [0, 11, 1, 0, 0, 0, 0, 6, 1, 0, 0]; 
+k_in{4} = [15, 27];
+k_in{5} = [16, 26];
+k_in{6} = [17, 25];
+k_in{7} = [13, 4];
+k_in{8} = [12, 5];
+k_in{9} = [11, 6];
+k_in{10} = [31, 18];
+k_in{11} = [30, 19];
+k_in{12} = [29, 20];
+k_in{13} = [14, 5];
+k_in{14} = [13, 6];
+k_in{15} = [12, 7];
+k_in{16} = [1, 0, 8];
+k_in{17} = [2, 2, 7];
+k_in{18} = [3, 2, 6]; 
+k_in{19} = [22, 6, 11];
+k_in{20} = [21, 6, 12];
+k_in{21} = [20, 7, 13];
+k_in{22} = [11, 22];
+k_in{23} = [12, 21]; 
+k_in{24} = [13, 20];
+k_in{25} = [35, 51]; 
+k_in{26} = [36, 50];
+k_in{27} = [37, 49];
 
 % 
-% Read election results and first round predictions
-fname = '2020_montgomery_formatted_computations.json';
+% Read election results
+fname = '2020_montgomery_formatted.json';
 election_computations = loadjson(fileread(fname));
-
-% % Read first tests, simply for writing into file. 
-fname = '2020_montgomery_tests.json';
-testing = loadjson(fileread(fname));
 
 races = fieldnames(election_computations.contests);
 
-for j=1:num_tests
-    % Find p-value, contest number is ceil(j/3)
-    i = ceil(j/3);
+for s=1:num_tests
     
-    % Generate winning vote distributions for each hypothesis
-    margin = election_computations.contests.(races{i}).info.margin;
-    CurrentTierStop = R2CurrentTier(margin,(1),n_in(j));
-    CurrentTierRisk = R2CurrentTier(0,(1),n_in(j));
+    %----------PART 0: error checking and initialization ---- %
+    % simple error checking
+    if (sum(k_in{s}) ~= n_in(s))
+        fprintf('%s, %d \n','error in sum votes', s)
+    end
     
-    %Compute p value
-    [pvalue, LR] = p_value(margin, (0), (0), CurrentTierStop, ...
-        CurrentTierRisk, n_in(j), k_in(j), 'Athena');
-    dvalue = 1/LR;
+    % contest number is ceil(s/3)
+    i = ceil(s/3);
     
-    % Write into test structure
-    testing.evaluate_risk.(sprintf('test%d',j)).audit_type = 'athena';
-    testing.evaluate_risk.(sprintf('test%d',j)).election = election_computations.name;
-    testing.evaluate_risk.(sprintf('test%d',j)).contest = races{i};
-    testing.evaluate_risk.(sprintf('test%d',j)).alpha = alpha; 
-    testing.evaluate_risk.(sprintf('test%d',j)).round_schedule = n_in(j)*ones(1); 
-    testing.evaluate_risk.(sprintf('test%d',j)).audit_observations = k_in(j)*ones(1); 
-    testing.evaluate_risk.(sprintf('test%d',j)).expected.passed = int8((pvalue <= alpha) && (dvalue <= delta)); 
-    testing.evaluate_risk.(sprintf('test%d',j)).expected.pvalue = pvalue; 
-    testing.evaluate_risk.(sprintf('test%d',j)).expected.delta = dvalue; 
+    % Initialize testing structure
+    testing.evaluate_risk.(sprintf('test%d',s)).audit_type = 'athena';
+    testing.evaluate_risk.(sprintf('test%d',s)).election = election_computations.name;
+    testing.evaluate_risk.(sprintf('test%d',s)).contest = races{i};
+    testing.evaluate_risk.(sprintf('test%d',s)).alpha = alpha; 
+    testing.evaluate_risk.(sprintf('test%d',s)).round_schedule = n_in(s); 
+    
+    candidates = fieldnames(election_computations.contests.(races{i}).tally);
+    for j=1:size(candidates)
+        testing.evaluate_risk.(sprintf('test%d',s)).audit_observations.(candidates{j}) = k_in{s}(j);
+    end
+    
+    votes = zeros(size(candidates)); % initialize vote vector
+    pvalue = votes; % initialize pvalues, same size as votes
+    dvalue = votes; % initialize dvalues, same size as votes
+    
+    % ----- PART I ---- Compute necessary properties -------
+    for j=1:size(candidates)
+        votes(j) = election_computations.contests.(races{i}).tally.(candidates{j});
+    end
+    % Find max votes
+    [votes_max, winner] = max(votes);
+
+    % For each candidate, including winner, compute margin
+    relevant_ballots = votes_max + votes;
+    margin = (votes_max-votes)./relevant_ballots;
+    
+    % ------ PART II ----- For each loser, compute pvalue---- 
+    losers = (1:size(candidates)); 
+    losers(winner) = []; % delete the winner
+    
+    % look at each losing candidate
+    for j = losers
+        % Generate winning vote distributions for each hypothesis
+        CurrentTierStop = R2CurrentTier(margin(j),(1), k_in{s}(winner) + k_in{s}(j));
+        CurrentTierRisk = R2CurrentTier(0,(1), k_in{s}(winner) + k_in{s}(j));
+    
+        %Compute p value
+        [pvalue(j), LR] = p_value(margin(j), (0), (0), CurrentTierStop, ...
+            CurrentTierRisk, k_in{s}(winner) + k_in{s}(j), ... 
+            k_in{s}(winner), 'Athena');
+            dvalue(j) = 1/LR;
+            
+        % Write into test structure     
+        testing.evaluate_risk.(sprintf('test%d',s)).expected.(candidates{j}).passed = int8((pvalue(j) <= alpha) && (dvalue(j) <= delta)); 
+        testing.evaluate_risk.(sprintf('test%d',s)).expected.(candidates{j}).pvalue = pvalue(j); 
+        testing.evaluate_risk.(sprintf('test%d',s)).expected.(candidates{j}).delta = dvalue(j); 
+    end
+    
+    % Summary p and d values
+    [pvalue_max, runnerup] = max(pvalue);
+    dvalue_max = max(dvalue);
+    
+    testing.evaluate_risk.(sprintf('test%d',s)).expected.passed = int8((pvalue_max <= alpha) && (dvalue_max <= delta)); 
+    testing.evaluate_risk.(sprintf('test%d',s)).expected.pvalue = pvalue_max; 
+    testing.evaluate_risk.(sprintf('test%d',s)).expected.delta = dvalue_max; 
 end
 
 % Write tests back into testing file
 txt = savejson('',testing);
-fname3 = '2020_montgomery_tests.json';
+fname3 = '2020_montgomery_pvalue_new_tests.json';
 fid = fopen(fname3, 'w');
 if fid == -1, error('Cannot create JSON file'); end
 fwrite(fid,txt,'char');
