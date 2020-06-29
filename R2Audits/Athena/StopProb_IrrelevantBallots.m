6c7200cfdb40208b85ec7b17cd3d41244bbcf02b
Claire Furtick <cfurtick@gwu.edu>
2020-06-29 10:37:30 -0400

    not working yet, stopping probabilities returned are 0

--- R2Audits/Athena/StopProb_IrrelevantBallots.m
+++ R2Audits/Athena/StopProb_IrrelevantBallots.m
@@ -1,4 +1,4 @@
-function [n, kmin, Stopping] = StopProb_IrrelevantBallots(margin, alpha, delta, ...
+function [n, kmin, Stopping] = StopProb_IrrelevantBallots(difference_fraction, alpha, delta, ...
 StopSched_prev, RiskSched_prev, CurrentTierStop, CurrentTierRisk, ... 
 n_last, k_last, max_draws, audit_method, irrelevant_fraction)
 
@@ -9,23 +9,23 @@ n_last, k_last, max_draws, audit_method, irrelevant_fraction)
 %
 % ---------------------------Inputs------------------------
 %
-%       margin:             fractional margin
-%       alpha:              fractional risk limit
-%       delta:              minimum value for Athena LR; not needed for 
-%                               other audit types
-%       StopSched_prev:     most recent Stop_Sched
-%       RiskSched_prev:     most recent RiskSched 
-%       CurrentTierStop:	most recent winner vote distribution for 
-%                               election with margin
-%       CurrentTierRisk:    most recent winner vote distribution for 
-%                               tied election
-%       n_last:             total number of ballots drawn so far
-%       k_last:             total number of winner votes drawn so far
-%       max_draws:          maximum number of ballots that can be 
-%                               drawn in all
-%       audit_method:       one of Arlo, Athena, Minerva, Metis
+%       difference_fraction:    fractional difference between winner and loser votes
+%       alpha:                  fractional risk limit
+%       delta:                  minimum value for Athena LR; not needed for 
+%                                   other audit types
+%       StopSched_prev:         most recent Stop_Sched
+%       RiskSched_prev:         most recent RiskSched 
+%       CurrentTierStop:        most recent winner vote distribution for 
+%                                   election with margin
+%       CurrentTierRisk:        most recent winner vote distribution for 
+%                                   tied election
+%       n_last:                 total number of ballots drawn so far
+%       k_last:                 total number of winner votes drawn so far
+%       max_draws:              maximum number of ballots that can be 
+%                                   drawn in all
+%       audit_method:           one of Arlo, Athena, Minerva, Metis
 %
-%       irrelevant_fraction: reported fraction of irrelevant ballots
+%       irrelevant_fraction:    reported fraction of irrelevant ballots
 %
 % -------------------------Outputs---------------------------
 %
@@ -34,9 +34,14 @@ n_last, k_last, max_draws, audit_method, irrelevant_fraction)
 %       Stopping:           corresponding stopping probability
 %
 
-% assumed fraction of winner votes, taking into account fraction of
-% irrelevant ballots reported
-p = ((1-irrelevant_fraction)+margin)/2;
+% assumed fraction of winner votes
+p = ((1-irrelevant_fraction)+difference_fraction)/2;
+
+% assumed fraction of loser votes
+l = 1-irrelevant_fraction-p;
+
+% margin calculation to be passed into R2CurrentTier and AthenaNextkmin
+margin = (p-l)/(1-irrelevant_fraction);
 
 % possible new total sample size
 n = (n_last+1:max_draws);
@@ -59,36 +64,50 @@ for j=1:max_draws-n_last % j is number of new ballots drawn
       % correct margin caluclations?
       NextTierStop = R2CurrentTier(margin,CurrentTierStop,j);
       NextTierRisk = R2CurrentTier(0,CurrentTierRisk,j);
-
-      % TODO: Is the AthenaNextkmin margin calculation also incorrect for
-      % irrelevant ballots?
-      kmin(j) = AthenaNextkmin(margin, alpha, delta, StopSched_prev, ...
-      RiskSched_prev, NextTierStop, NextTierRisk, n(j), audit_method);
    end
+   % TODO: Is the AthenaNextkmin margin calculation also incorrect for
+   % irrelevant ballots?
+   kmin(j) = AthenaNextkmin(margin, alpha, delta, StopSched_prev, ...
+   RiskSched_prev, NextTierStop, NextTierRisk, n(j), audit_method);
+  
+%    for i=1:size(kmin,2)
+%      fprintf('%d', kmin(i));
+%      fprintf('\n');
+%    end
 end
 
 %---------------Compute Stopping----------------%
 
 % Assuming not Arlo right now... 
+
 for j=1:max_draws-n_last % j is number of new ballots drawn
 
     % Round is large enough for non-zero stopping probability
     if kmin(j) <= n(j)
         
         Final_stop_prob = 0;
-        for i=0:j
+        for i=0:j % i is the number of irrelevant ballots, j-i is the number of relevant ballots
             
-            % TO THINK ABOUT: Maybe this for loop replaced by a
-            % trinomial distribution that varies over votes for winner...?
+            % initualize the stopping probability
             Stop_prob=0;
-            for k=kmin(j-i)-k_last:j-i %TODO: check if j-i is correct!
-                
-                % Calculate the probability of drawing k winner ballots,
-                % j-k-i loser ballots, and i irrelevant ballots
-                Stop_prob = Stop_prob + TrinomialDistribution(p,irrelevant_fraction,k,j-k-i,i,j);
-            end
             
-            Final_stop_prob = Final_stop_prob + Stop_prob;
+            % There is no chance of stopping if...
+            %   - 0 of new ballots drawn are relevant
+            %   - the number of relevant votes drawn is not large enough,
+            %       meaning its corresponding kmin value returned is larger
+            %       than itself   
+            if j-i > 0 && kmin(j-i) <= n(j-i)
+                for k=kmin(j-i)-k_last:j-i 
+                
+                    % Calculate the probability of drawing k winner ballots,
+                    % j-k-i loser ballots, and i irrelevant ballots
+                    Stop_prob = Stop_prob + TrinomialDistribution(p,l,irrelevant_fraction,k,j-k-i,i,j);
+                end
+                %
+                %invalid_prob = irrelevant_fraction^i;
+                %Stop_prob = 1 - (invalid_prob * binocdf(kmin(j-i)-k_last,j-i,p));
+                Final_stop_prob = Final_stop_prob + Stop_prob;
+            end
         end 
             
         Stopping(j) = Final_stop_prob;
--- R2Audits/Athena/TrinomialDistribution.m
+++ R2Audits/Athena/TrinomialDistribution.m
@@ -1,9 +1,8 @@
-function prob = TrinomialDistribution(winner_fraction, irrelevant_fraction, ... 
+function final_prob = TrinomialDistribution(winner_fraction, loser_fraction, irrelevant_fraction, ... 
 winner_ballots, loser_ballots, irrelevant_ballots, total_ballots)
 
-    loser_fraction = 1 - winner_fraction - irrelevant_fraction;
-    prob = (winner_fraction^(winner_ballots))*(loser_fraction^(loser_ballots))*(irrelevant_fraction^(irrelevant_ballots));
-    num_orderings = (factorial(total_ballots)) / ((factorial(winner_ballots))*(factorial(loser_ballots))*(factorial(irrelevant_ballots)));
-    prob = prob * num_orderings;
+    prob = (winner_fraction^winner_ballots)*(loser_fraction^loser_ballots)*(irrelevant_fraction^irrelevant_ballots);
+    num_orderings = factorial(total_ballots) / ((factorial(winner_ballots))*(factorial(loser_ballots))*(factorial(irrelevant_ballots)));
+    final_prob = prob * num_orderings;
 
 end
