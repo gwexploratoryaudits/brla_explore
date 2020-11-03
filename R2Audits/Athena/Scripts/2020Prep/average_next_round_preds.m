eb59a3b71acd829626d47a3daf006400da989a9e
Poorvi Vora <poorvi@gwu.edu>
2020-11-03 13:58:08 -0500

    Get second round data into array for plots

--- R2Audits/Athena/Scripts/2020Prep/average_next_round_preds.m
+++ R2Audits/Athena/Scripts/2020Prep/average_next_round_preds.m
@@ -15,25 +15,32 @@ audit_method_B = 'EoR';
 
 for i=1:size(states,1)
     margin = abs(election_results.(states{i}).contests.presidential.margin);
+    margin_many(i) = margin;
     % doing only for 20% or larger margins right now
     if (margin > 0.1)
         second_round.(states{i}).margin = margin;
         % First write raw first round sizes
         n_in = election_results.(states{i}).contests.presidential.Athena_pv_raw;
         n_in_B = election_results.(states{i}).contests.presidential.Arlo_pv_raw;
+        n_in_many(i) = n_in;
+        n_in_B_many(i) = n_in_B;
         % Now compute second round sizes. 
         call_first_round_averages
         % Write round sizes
         second_round.(states{i}).round_sizes_Minerva = [n_in, next_round_size];
         second_round.(states{i}).round_sizes_EoR = [n_in_B, next_round_size_B];
+        next_round_many(i) = next_round_size;
+        next_round_B_many(i) = next_round_size_B;
         second_round.(states{i}).second_round_sizes_factor_Minerva = next_round_size/n_in;
         second_round.(states{i}).second_round_sizes_factor_EoR = next_round_size_B/n_in_B;
         second_round.(states{i}).second_round_size_factor_incremental = ...
             (next_round_size_B-n_in_B)/(next_round_size-n_in);
         second_round.(states{i}).average_round_size_Minerva = ...
             StopSched(1,1)*n_in +(1-StopSched(1,1))*next_round_size; 
+        average_many(i) = StopSched(1,1)*n_in +(1-StopSched(1,1))*next_round_size; 
         second_round.(states{i}).average_round_size_EoR = ...
             StopSched_B(1,1)*n_in_B +(1-StopSched_B(1,1))*next_round_size_B;
+        average_B_many(i) = StopSched_B(1,1)*n_in_B +(1-StopSched_B(1,1))*next_round_size_B;
         second_round.(states{i}).average_round_size_factor = ...
             (StopSched_B(1,1)*n_in_B ...
             +(1-StopSched_B(1,1))*next_round_size_B)/(StopSched(1,1)*n_in ...
