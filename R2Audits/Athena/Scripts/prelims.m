ce9a9b82e771d97926e997317b777a696c13e746
Poorvi Vora <poorvi@gwu.edu>
2020-11-12 20:08:51 -0500

    new prelims files

--- R2Audits/Athena/Scripts/prelims.m
+++ R2Audits/Athena/Scripts/prelims.m
@@ -1,4 +1,11 @@
-margin = (2453876-2358432)/(2453876+2358432);
+%margin = (3365775-3321037)/(3365775+3321037);
+% 155976      114648       90719
+%margin = 0.01;
+% 69837       51231       40702
+% margin = 100/7000;
+% 34249       25154       19958
+
+margin = (1270718-978667)/(1270718+978667);
 alpha = 0.1;
 delta = 1.0;
 StopSched_prev = (0);
@@ -6,7 +13,7 @@ RiskSched_prev = (0);
 CurrentTierRisk = (1);
 CurrentTierStop = (1);
 n_prev=0;
-percentiles = [0.9, 0.8, 0.7];
+percentiles = [0.9];
 n_prev=0;
 max_round_size = 10000;
 tolerance = 0.0001;
