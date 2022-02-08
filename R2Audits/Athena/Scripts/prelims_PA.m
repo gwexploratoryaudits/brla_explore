92b7a2a55ec93e15a1c195ebaae9967be4318709
Poorvi Vora <poorvi@gwu.edu>
2022-02-08 17:59:24 -0500

    NextRoundSizeOld?

---
+++ R2Audits/Athena/Scripts/prelims_PA.m
@@ -0,0 +1,16 @@
+margin = (3459923-3378263)/(3459923+3378263);
+scale = (3459923+3378263+79397)/(3459923+3378263);
+margin2 = (3458229-3377674)/(3458229+3377674);
+scale2 = (3458229+3377674+79380)/(3458229+3377674);
+alpha = 0.1;
+delta = 1.0;
+StopSched_prev = (0);
+RiskSched_prev = (0);
+CurrentTierRisk = (1);
+CurrentTierStop = (1);
+n_prev=0;
+percentiles = [0.9, 0.8, 0.7];
+n_prev=0;
+max_round_size = 10000;
+tolerance = 0.0001;
+audit_method = 'Minerva';
