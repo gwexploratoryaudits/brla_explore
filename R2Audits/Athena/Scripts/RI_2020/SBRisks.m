9e321a16b13d60d3358ba88800e2b9f7503f77c9
Poorvi Vora <poorvi@gwu.edu>
2022-02-11 12:05:24 -0500

    compute risks

---
+++ R2Audits/Athena/Scripts/RI_2020/SBRisks.m
@@ -0,0 +1,9 @@
+function pvalues = SBRisks(margin, alpha, votes)
+%
+% pvalues = SBRisks(margin, alpha, votes)
+%
+% This function returns a vector of SB p-values of the same length as 
+% votes. 
+%
+% It updates the p-value multiplying by p/0.5 for winner votes and
+% (1-p)/0.5 for loser votes. Nothing for irrelevants. 
