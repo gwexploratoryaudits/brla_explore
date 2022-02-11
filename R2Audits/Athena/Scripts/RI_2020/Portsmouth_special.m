7882f9b16c071d423c2d1b4e4a5903aaa20b2c6f
Poorvi Vora <poorvi@gwu.edu>
2022-02-11 18:51:03 -0500

    computations at RI audit

--- R2Audits/Athena/Scripts/RI_2020/Portsmouth_special.m
+++ R2Audits/Athena/Scripts/RI_2020/Portsmouth_special.m
@@ -1,17 +1,17 @@
 margin = (2391-1414)/(2391+1414);
 alpha = 0.1;
-[Minerva_first_round, ~, ~, ~, ~] = NextRoundSize(margin, alpha, [], (0), (0), ...
-            (1), (1), 0, 0, (0.9), 500, 0.0001); %109
-[Minerva_first_round_2, ~, ~, ~, ~] = NextRoundSize(margin, alpha, [], (0), (0), ...
-            (1), (1), 0, 0, (0.95), 500, 0.0001); %139
 
-%--- For SB round size
-[~, ~, n1, kmin1] = B2BRAVOkmin(margin, alpha);
-[StopSched1, StopValue1, ExpectedBallots1] = B2Risks(margin, [], n1, kmin1, 0);
-stopping_values = StoppingPercentiles(n1, StopSched1, (0.9)); % 148 + 36 percent over Minerva
-stopping_values_2 = StoppingPercentiles(n1, StopSched1, (0.95)); % 197 + 33 percent over Minerva
+this_draw = 140;
 
-%--- For EoR round size
-[next_round_EoR, ~, sprob] = ...
-           BinaryNextRoundSizeGranular(margin, alpha, 1.0, (0), (0), (1), ...
-               (1), 0, (0.95), 500, 0.0001, 'EoR'); %288 more than 100% over Minerva
+CurrentTierStop = binopdf(0:this_draw,this_draw, 0.5*(1+margin));
+CurrentTierRisk = binopdf(0:this_draw,this_draw, 0.5);
+StopSched = (0);
+RiskSched = (0);
+this_k=81;
+
+[tail_ratio, LR] = p_value(margin, StopSched, ...
+                RiskSched, CurrentTierStop, CurrentTierRisk, this_draw, ...
+                this_k, 'Minerva');
+
+sigma = 1/LR;
+            
--- R2Audits/Athena/Scripts/RI_2020/SBRisks.m
+++ R2Audits/Athena/Scripts/RI_2020/SBRisks.m
@@ -7,3 +7,6 @@ function pvalues = SBRisks(margin, alpha, votes)
 %
 % It updates the p-value multiplying by p/0.5 for winner votes and
 % (1-p)/0.5 for loser votes. Nothing for irrelevants. 
+
+for i=1:size(votes)
+    
